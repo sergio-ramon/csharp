db686153abdbe639f6eec962b702b1a5ba7e179f
Sergio Ramon <sergioramon.sr98@hotmail.com>
2024-10-15 13:32:03 -0300

    6º Commit - Arquivos que foram alterados automaticamente

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda2180c0ef9a645ab4b8bcbf662ee610c264de1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15743cc2fb55cc3bd8fd50a80052be21809b2d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
