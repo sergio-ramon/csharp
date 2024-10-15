d5030423566044c2d97fd486e9bcad408ae32029
Sergio Ramon <sergioramon.sr98@hotmail.com>
2024-10-15 13:46:04 -0300

    7º Commit - Inclusão das letras do alfabeto e verificação de maiúscula no Conversor Binário

--- ConversorBinario/Program.cs
+++ ConversorBinario/Program.cs
@@ -2,18 +2,90 @@
 int[] valBinario = [0, 0, 0, 0, 0, 0, 0, 0];
 int valDecimal = 0;
 
-Console.WriteLine("Digite uma letra entre a, b ou c: ");
+Console.WriteLine("Digite uma letra do alfabeto: ");
 valCaractere = Convert.ToChar(Console.Read());
 
-switch (valCaractere) {
-    case 'a':
+switch (Char.ToUpper(valCaractere)) {
+    case 'A':
         valDecimal = 65;
         break;
-    case 'b':
+    case 'B':
         valDecimal = 66;
         break;
-    case 'c':
+    case 'C':
         valDecimal = 67;
+        break;
+    case 'D':
+        valDecimal = 68;
+        break;
+    case 'E':
+        valDecimal = 69;
+        break;
+    case 'F':
+        valDecimal = 70;
+        break;
+    case 'G':
+        valDecimal = 71;
+        break;
+    case 'H':
+        valDecimal = 72;
+        break;
+    case 'I':
+        valDecimal = 73;
+        break;
+    case 'J':
+        valDecimal = 74;
+        break;
+    case 'K':
+        valDecimal = 75;
+        break;
+    case 'L':
+        valDecimal = 76;
+        break;
+    case 'M':
+        valDecimal = 77;
+        break;
+    case 'N':
+        valDecimal = 78;
+        break;
+    case 'O':
+        valDecimal = 79;
+        break;
+    case 'P':
+        valDecimal = 80;
+        break;
+    case 'Q':
+        valDecimal = 81;
+        break;
+    case 'R':
+        valDecimal = 82;
+        break;
+    case 'S':
+        valDecimal = 83;
+        break;
+    case 'T':
+        valDecimal = 84;
+        break;
+    case 'U':
+        valDecimal = 85;
+        break;
+    case 'V':
+        valDecimal = 86;
+        break;
+    case 'W':
+        valDecimal = 87;
+        break;
+    case 'X':
+        valDecimal = 88;
+        break;
+    case 'Y':
+        valDecimal = 89;
+        break;
+    case 'Z':
+        valDecimal = 90;
+        break;
+    default:
+        Console.WriteLine("Letra inválida");
         break;
 }
 
--- ConversorBinario/obj/Debug/net8.0/ConversorBinario.AssemblyInfo.cs
+++ ConversorBinario/obj/Debug/net8.0/ConversorBinario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversorBinario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15743cc2fb55cc3bd8fd50a80052be21809b2d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db686153abdbe639f6eec962b702b1a5ba7e179f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversorBinario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversorBinario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
