baf10a8155e7318dd6aa36d5e0357f792c49d238
Sergio Ramon <sergioramon.sr98@hotmail.com>
2024-10-16 11:33:43 -0300

    Adição - Projeto de estudo ParOuImpar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿class ParOuImpar {
+    public static int Main() {
+        int num;
+
+        do {
+            Console.WriteLine("Digite um número positivo: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            if (num % 2 == 0 && num > 0) {
+                Console.WriteLine($"O número {num} é par!");
+            } else {
+                Console.WriteLine($"O número {num} é impar!");
+            }
+        } while (num > 0);
+
+        return 0;
+    }
+}
