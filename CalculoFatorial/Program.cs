d71da84e7731c610d26f9b90ab601c868908fb6b
Sergio Ramon <sergioramon.sr98@hotmail.com>
2024-10-16 14:07:29 -0300

    Adição - Projeto de Calculo Fatorial

---
+++ CalculoFatorial/Program.cs
@@ -0,0 +1,20 @@
+﻿class CalculoFatorial {
+    public static int Fatorial(int x) {
+        if (x == 0) {
+            return 1;
+        } else {
+            return x * Fatorial(x-1);
+        }
+    }
+
+    public static int Main() {
+        int num;
+
+        Console.WriteLine("Digite um número: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Fatorial(num));
+
+        return 0;
+    }
+}
